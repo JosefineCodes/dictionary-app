6832c00c7d907cd6883a66ae07efadfd38ac96a9
Phinni <93153106+Phinni@users.noreply.github.com>
2022-08-14 16:43:42 +0200

    made search results be displayed in Results component with loop through meaning.definitions

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -8,14 +8,13 @@ export default function Dictionary() {
   let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data[0]);
     setResults(response.data[0]);
   }
 
   function search(event) {
     event.preventDefault();
 
-    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
 
--- src/Meaning.js
+++ src/Meaning.js
@@ -2,5 +2,20 @@ import React from "react";
 
 export default function Meaning(props) {
   console.log(props.meaning);
-  return <div className="meaning">Hello from Meaning.</div>;
+  return (
+    <div className="Meaning">
+      <h4>{props.meaning.partOfSpeech}</h4>
+      {props.meaning.definitions.map(function (definition, index) {
+        return (
+          <div key={index}>
+            <p>
+              {definition.definition}
+              <br />
+              <em>{definition.example}</em>
+            </p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
